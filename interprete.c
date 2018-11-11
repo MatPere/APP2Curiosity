978a88ae66a5946bf76db414e58e293a9bb41b60
MatPere <32298732+MatPere@users.noreply.github.com>
2018-11-11 11:14:32 +0100

    nouvel interprete fonctionnel
    Il reste un bug d'affichage de la séquence

--- interprete.c
+++ interprete.c
@@ -7,6 +7,7 @@
 #endif
 #include "listes.h"
 #include "curiosity.h"
+#include "interprete.h"
 
 /*
  *  Auteur(s) :
@@ -26,151 +27,173 @@ void stop (void)
 #endif
 }
 
+cellule_t * condition_ou_boucle(cellule_t * act, int *n, int *fin, bool debug){
+  int profondeur = 1 ;
 
 
-void traiter_condition(sequence_t * seq, int n){
-
-  int profondeur ;
-  int non_vide ;
-  cellule_t *courant = seq->tete->suivant;
-  cellule_t *premier = courant;
-  cellule_t *dernier = courant;
-
-  if (n==0){
-    profondeur = 1;
-    while (profondeur > 0) {
-      if (courant->command=='{') profondeur++;
-      if (courant->command=='}') profondeur--;
-      courant=courant->suivant;
-    }
-    courant=courant->suivant ;
-    profondeur = 1;
-    premier=courant ;
-    if (premier->command=='}'){
-      non_vide = 0;
-    } else {
-      while (profondeur > 0) {
-        if (courant->command=='{') profondeur++;
-        if (courant->command=='}') profondeur--;
-        if ((courant->suivant->command=='}')&&(profondeur==1)) dernier = &*courant;
-        courant=courant->suivant;
-      }
-      non_vide = 1;
-    }
- } else {
-   profondeur = 1;
-   premier=courant ;
-   if (premier->command=='}'){
-     non_vide = 0;
-   } else {
-     while (profondeur > 0) {
-       if (courant->command=='{') profondeur++;
-       if (courant->command=='}') profondeur--;
-       if ((courant->suivant->command=='}')&&(profondeur==1)) dernier = &*courant;
-       courant=courant->suivant;
-     }
-     non_vide = 1;
-   }
-     courant=courant->suivant ;
-     profondeur = 1;
-     while (profondeur > 0) {
-       if (courant->command=='{') profondeur++;
-       if (courant->command=='}') profondeur--;
-       courant=courant->suivant;
-     }
-
- }
-
- if (non_vide){
-    seq->tete->suivant = premier;
-    dernier->suivant = courant->suivant;
-
-  } else seq->tete->suivant=courant->suivant;
+  cellule_t * debut = act ;
+  act = act->suivant;
+  while (profondeur > 0) {
+    if (act->command=='{') profondeur++;
+    if (act->command=='}') profondeur--;
+    act=act->suivant;
+  }
+  if (act->command=='{') return traiter_condition(debut, n, fin, debug) ;
+  else return traiter_boucle(debut, n ,fin, debug);
 
 }
 
 
+cellule_t * traiter_boucle(cellule_t * act, int *n, int *fin, bool debug) {
+
+  int profondeur = 1 ;
+  int iter ;
+  act = act->suivant;
+  cellule_t * debut = act ;
+  while (profondeur > 0) {
+    if (act->command=='{') profondeur++;
+    if (act->command=='}') profondeur--;
+    act=act->suivant;
+  }
+  iter = act->suivant->command ;
+  for (int i=0; i<iter; i++){
+    act=debut;
+    while (act->command!='}'){
+      act = traiter(act, n, fin, debug) ;
+      if (act == NULL) return NULL ;
+    }
+  }
+  return act->suivant->suivant->suivant ;
+}
+
+cellule_t * traiter_condition(cellule_t * act, int *n, int *fin, bool debug){
+  act = act->suivant;
+  int profondeur = 1 ;
+  if (*n==0){
+    while (profondeur > 0) {
+      if (act->command=='{') profondeur++;
+      if (act->command=='}') profondeur--;
+      act=act->suivant;
+    }
+    act=act->suivant ;
+    while (act->command!='?') {
+      act = traiter(act, n, fin, debug) ;
+      if (act == NULL) return NULL ;
+
+    }
+    return act->suivant;
+
+
+ } else {
+
+
+   while (act->command!='}') {
+     act = traiter(act, n, fin, debug) ;
+     if (act == NULL) return NULL ;
+
+   }
+
+   act=act->suivant->suivant ;
+
+
+   while (profondeur > 0) {
+     if (act->command=='{') profondeur++;
+     if (act->command=='}') profondeur--;
+     act=act->suivant;
+   }
+   return act->suivant;
+ }
+}
+
+
+
+
+cellule_t * traiter(cellule_t *act, int * n, int * fin, bool debug){
+  if (act==NULL && *fin==-1) *fin = CIBLERATEE ;
+  if (act==NULL) return NULL ;
+  if (*fin != -1) return NULL ;
+  else {
+    char commande = act->command ;
+
+    if ((commande>='0')&&(commande<='9')) {
+      *n = commande - '0';
+
+    } else if ((commande=='a')||(commande=='A')) {
+        switch (avance()) {
+            case VICTOIRE: *fin = VICTOIRE; return NULL ;
+            case RATE: *fin = RATE; return NULL ;
+            case REUSSI: break;
+        }
+    } else if (commande=='{') {
+        return condition_ou_boucle(act, n, fin, debug);
+    } else if ((commande=='g')||(commande=='G')) {
+        gauche();
+    } else if ((commande=='d')||(commande=='D')) {
+        droite();
+    } else if ((commande=='p')||(commande=='P')) {
+        pose(*n);
+    } else if ((commande=='B')||(commande=='}')||(commande=='?')) {
+        //rien
+
+    } else if ((commande=='m')||(commande=='M')) {
+        *n=mesure(*n);
+        printf("Résultat de la mesure : ");
+        switch (*n) {
+          case 0 : {
+            printf("rien\n" );
+            break ;
+          }
+          case 1 : {
+            printf("marque\n" );
+            break ;
+          }
+          case 2 : {
+            printf("eau\n" );
+            break ;
+          }
+          case 3 : {
+            printf("rocher\n" );
+            break ;
+          }
+        }
+
+
+    } else {
+        printf ("Commande inconnue: %c\n", commande);
+    }
+
+
+    afficherCarte();
+    printf("Valeur : %d\n",*n);
+    printf ("Programme : ");
+    sequence_t seq;
+    seq.tete = act ;
+    afficher(&seq);
+    printf ("\n");
+    return act->suivant;
+    if (debug) stop();
+  }
+}
+
 int interprete (sequence_t* seq, bool debug)
 {
-    // Version temporaire a remplacer par
-    //  - une lecture des commandes dans la liste chainee
-    // suivie par
-    //  - une interpretation des commandes lues (partie fournie)
-    char argument = 0;
-    char commande = 0;
+
+    int n = 0;
+    cellule_t * act = seq ->tete ;
+    int fin =-1 ; //Variable qui transporte l'information de victoire
 
 
-     /* debug = True À enlever par la suite et utiliser "-d" sur la ligne de commandes */
+
 
     printf ("Programme : ");
     afficher(seq);
     printf ("\n");
     if (debug) stop();
-    // À partir d'ici, beaucoup de choses à modifier dans la suite.
-
-     //à modifier: premiere commande de la sequence
-
-    while ( seq->tete != NULL) {
-        commande = seq->tete->command ;
-
-        if ((commande>='0')&&(commande<='9')) {
-          argument = commande - '0';
-
-        } else if ((commande=='a')||(commande=='A')) {
-            switch (avance()) {
-                case VICTOIRE: return VICTOIRE;
-                case RATE: return RATE;
-                case REUSSI: break;
-            }
-        } else if (commande=='{') {
-            traiter_condition(seq, argument);
-        } else if ((commande=='g')||(commande=='G')) {
-            gauche();
-        } else if ((commande=='d')||(commande=='D')) {
-            droite();
-        } else if ((commande=='p')||(commande=='P')) {
-            pose(argument);
-
-        } else if ((commande=='m')||(commande=='M')) {
-            argument=mesure(argument);
-            printf("Résultat de la mesure : ");
-            switch (argument) {
-              case 0 : {
-                printf("rien\n" );
-                break ;
-              }
-              case 1 : {
-                printf("marque\n" );
-                break ;
-              }
-              case 2 : {
-                printf("eau\n" );
-                break ;
-              }
-              case 3 : {
-                printf("rocher\n" );
-                break ;
-              }
-            }
 
 
-        } else {
-            printf ("Commande inconnue: %c\n", commande);
-        }
-
-        seq->tete= seq->tete->suivant;
-        afficherCarte();
-        printf("Valeur : %d\n",argument);
-        printf ("Programme : ");
-        afficher(seq);
-        printf ("\n");
-        if (debug) stop();
+    while ( act != NULL) {
+      act = traiter(act, &n, &fin, debug);
+      if (fin != -1) return fin ;
     }
-
-
-
-    /* Si on sort de la boucle sans arriver sur le @
-     * c'est raté :-( */
-
     return CIBLERATEE;
 }
--- interprete.h
+++ interprete.h
@@ -1,6 +1,9 @@
 #ifndef INTERPRETE_H
 #define INTERPRETE_H
-
+cellule_t * traiter(cellule_t * act, int *n, int *fin, bool debug) ;
 int interprete (sequence_t* seq, bool debug);
+cellule_t * traiter_boucle(cellule_t * act, int *n, int *fin, bool debug);
+cellule_t * traiter_condition(cellule_t * act, int *n, int *fin, bool debug);
+cellule_t * condition_ou_boucle(cellule_t * act, int *n, int *fin, bool debug);
 
 #endif
